ee59c60bb0e16071f3f8f2b1d3e887c74f683a34
bunatl <lukas.bunat@gmail.com>
2020-09-26 16:21:51 +0200

    router configured

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,37 @@
 import React from 'react';
-// import { BrowserRouter, Route, Link } from "react-router-dom";
+import { BrowserRouter, Route, Link } from "react-router-dom";
 
 // import styles
 import './styles/styles.scss'
 
-// import components
-import Header from './components/Header'
-import Content from './components/Content'
+// header imports
+import { Quote, SharingInfo } from './components/Header'
+
+// content imports
+import Clicks from './components/content/Clicks'
+import TotalClicks from './components/content/TotalClicks'
+import Leaderboard from './components/content/Leaderboard'
+import Motivation from './components/content/Motivation'
+
+// footer import
 import Footer from './components/Footer'
 
 function App() {
   return (
-    <>
-      <Header />
-      <Content />
-      <Footer />
-    </>
+    <BrowserRouter>
+      <header>
+        <div className="title"><h1>stfuandclick.com</h1></div>
+        <Route path="/" exact component={Quote} />
+        <Route path="/:id" component={SharingInfo} />
+      </header>
+      <main>
+        <Route path="/" exact component={Clicks} />
+        <Route path="/:id" component={TotalClicks} />
+        <Route path="/" component={Leaderboard} />
+        <Route path="/" component={Motivation} />
+      </main>
+      <Route path="/" component={Footer} />
+    </BrowserRouter>
   );
 }
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 
-
-function Header() {
+export function Quote() {
     return (
-        <header>
-            <div className="title"><h1>stfuandclick.com</h1></div>
-            <div className="quote">
-                <div className="text">"It's really simple,  you just need to click as fast as you can"</div>
-                <div className="author">- anonymous</div>
-            </div>
-            <div className="share"></div>
-        </header>
+        <div className="quote">
+            <div className="text">"It's really simple,  you just need to click as fast as you can"</div>
+            <div className="author">- anonymous</div>
+        </div>
     );
 }
 
-export default Header;
+export function SharingInfo() {
+    return (
+        <div className="share">
+            <div>Clicking for team <span>Lukas</span></div>
+            <div>Too lazy to click? Let your pals click for you: <div>prefix/Lukas</div></div>
+        </div>
+
+    );
+}
+
